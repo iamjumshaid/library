9b30dc177979c5baea7a25445516f031ea3bcdf3
iamjumshaid <iamjumshaidkhan@gmail.com>
2022-07-05 13:00:29 +0500

    Add JS for mobile navbar

--- app/components/page/navigation_component.rb
+++ app/components/page/navigation_component.rb
@@ -4,4 +4,17 @@ class Page::NavigationComponent < ApplicationComponent
   def render?
     !helpers.devise_controller?
   end
+
+  def menu_items
+    @menu_items ||= [
+      { text: 'Dashboard', controller: 'home', url: '#' }, 
+      { text: 'Library', controller: '', url: '#' }, 
+      { text: 'My Books', controller: '', url: '#' } 
+    ]
+  end
+
+  def nav_item_classes(controller)
+    controller == helpers.controller_name ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white'
+  end
+  
 end
