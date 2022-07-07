a5e9bd090497f2e801f8fce25b33388e32a9d3df
iamjumshaid <iamjumshaidkhan@gmail.com>
2022-07-07 18:02:54 +0500

    Fixed book readable condition

--- app/components/book/book_component.rb
+++ app/components/book/book_component.rb
@@ -4,6 +4,6 @@ class Book::BookComponent < ApplicationComponent
   option :book, model: 'Book'
 
   def readable?
-    helpers.controller_name == 'books'
+    helpers.controller_name == 'books' && book.ebook.attached?
   end
 end
